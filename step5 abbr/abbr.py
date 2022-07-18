894e4b59cb069913d9bd3300ca12c43c5fef700e
xwZhang <910452300@qq.com>
2022-07-18 15:15:58 +0800

    update abbr

--- step5 abbr/abbr.py
+++ step5 abbr/abbr.py
@@ -4,7 +4,8 @@ from tqdm import tqdm
 import random
 def reverse(s):
     return ' '.join(s.split(' ')[::-1])
-#合并主语宾语数据
+
+
 def hebing():
     with open('D:\\short_comment\\step3 completion\\codebert\\result\\codebert_sub\\code\\model\\java\\yizhi_result\\result_sub_all_succ_or_fail.txt','r',encoding='utf-8')as f:
         data=f.readlines()
@@ -34,11 +35,23 @@ def hebing():
         
         for i in range(len(nees)):
             fff.write(nees[i].replace('\n','')+'\n')
-#随即选择
+def hebing2():
+    with open('D:\\short_comment\\step3 completion\\codebert\\result\\codebert_pre\\code\\model\\java\\yizhi_result\\result_pre_all_succ_or_fail.txt','r',encoding='utf-8')as f:
+        data=f.readlines()
+        datts=[]
+        for dat in data:
+            datt=dat.replace('\n','')+','+'pre'+'\n'
+            datts.append(datt)
+    with open('pre_test.txt','w',encoding='utf-8')as ff:
+        for i in range(len(datts)):
+            ff.write(datts[i])
 def sample():
     with open('sub_obj_test.txt','r',encoding='utf-8')as f:
         data=f.readlines()
         random.shuffle(data)
+        
+        
+        
     flag=True
     while(flag):        
         sample_data=random.sample(data, 384)
@@ -87,9 +100,52 @@ def sample():
         for i in range(len(sample_data)):
             ff.write(sample_data[i])
     # return sample_data
-#缩写获取
-def get_abbr():
-    with open('sample_sub_obj.csv','r',encoding='utf-8')as f:
+def sample2():
+    with open('pre_test.txt','r',encoding='utf-8')as f:
+        data=f.readlines()
+        random.shuffle(data)
+    flag=True
+    while(flag):        
+        sample_data=random.sample(data, 126)
+ 
+        pre_success=0
+        # obj_success=0
+        pre_fail=0
+        # obj_fail= 0  
+        for dat in sample_data:
+            nee=dat.split(',')[-1].replace('\n','')
+            choice=dat.split(',')[-2]
+            if nee == 'pre' and choice == 'success':
+                pre_success +=1
+            elif nee == 'pre' and choice == 'fail':
+                pre_fail +=1
+
+            else:
+                print(dat)
+        print('--pre_success: ' + str(pre_success))
+        # print('--obj_success: ' + str(obj_success))
+        print('--pre_fail: ' + str(pre_fail))
+        # print('--obj_fail: ' + str(obj_fail))    
+        if pre_success >=75:
+            flag= False
+            continue
+    # sub=0
+    # obj=0    
+    # for dat in sample_data:
+    #     nee=dat.split(',')[-1].replace('\n','')
+    #     if nee == 'sub' :
+    #         sub+=1
+    #     elif nee =='obj':
+    #         obj+=1
+    #     else:
+    #         print(dat)
+    # print('--sub_num: ' + str(sub))
+    # print('--obj_num: ' + str(obj))
+    with open('sample_pre.csv','w',encoding='utf-8')as ff:
+        for i in range(len(sample_data)):
+            ff.write(sample_data[i])
+def get_abbr(a):
+    with open(f'sample_{a}.csv','r',encoding='utf-8')as f:
         data=f.readlines()
         codes=[]
         comms=[]
@@ -97,6 +153,9 @@ def get_abbr():
         abbrs=[]
         for dat in data:
             code=dat.split(',')[1]
+            # for c in string.punctuation:
+            #     code1=code.replace(c,"")
+            # code1=code.translate(None, string.punctuation)
             code1=re.sub(r'[^\w\s]','',code)
             code1=re.sub(r'_','',code1)
             # print(code1)
@@ -108,6 +167,7 @@ def get_abbr():
             abbr=[]
             for wor in words:
                 a=wor
+                # Word.get(a)
                 try:
                     Word.get(a)
                 except:
@@ -121,17 +181,27 @@ def get_abbr():
             abbrs.append(abbr_each)
         # print(abbr)
     return abbrs     
-def write_to_file(abbrs,sample_data):
+def write_to_file(a,abbrs,sample_data):
     
-    with open('abbr_sample_sub_obj.csv','w',encoding='utf-8')as ff:
+    with open(f'abbr_sample_{a}.csv','w',encoding='utf-8')as ff:
         for i in range(len(abbrs)):
+            
             ff.write(sample_data[i].replace('\n','')+','+abbrs[i]+'\n')
                     
 if __name__=='__main__':
-    # hebing()
-    # sample()
-    abbrs= get_abbr()       
-    with open('sample_sub_obj.csv','r',encoding='utf-8')as ff:
+    # # hebing()
+    # # sample()
+    # a='sub_obj'
+    # abbrs= get_abbr(a)       
+    # with open('sample_sub_obj.csv','r',encoding='utf-8')as ff:
+    #     sample_data=ff.readlines()
+    # write_to_file(a,abbrs, sample_data)
+
+
+    # hebing2()
+    # sample2()
+    a='pre'
+    abbrs= get_abbr(a)       
+    with open('sample_pre.csv','r',encoding='utf-8')as ff:
         sample_data=ff.readlines()
-           
-    write_to_file(abbrs, sample_data)
+    write_to_file(a,abbrs, sample_data)    
